0b43d750db84e0f0277d59075444ceecb662d283
DESKTOP-E9LVFLM\anddroid <abrarahmad3361@gmail.com>
2018-06-22 12:43:15 +0500

    tabbed

--- app/src/main/java/com/azuresamples/msalandroidapp/Main2Activity.java
+++ app/src/main/java/com/azuresamples/msalandroidapp/Main2Activity.java
@@ -1,10 +1,14 @@
 package com.azuresamples.msalandroidapp;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Main2Activity extends AppCompatActivity {
+    //private SectionsPagerAdapter mSectionsPagerAdapter;
 
+
+    private ViewPager mViewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
